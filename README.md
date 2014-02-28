b84e67d8e561e46e1063752f224642b54b80897b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-03-01 00:08:29 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::CPANJ - It's new $module
+
+# SYNOPSIS
+
+    use Acme::CPANJ;
+
+# DESCRIPTION
+
+Acme::CPANJ is ...
+
+# LICENSE
+
+Copyright (C) Tokuhiro Matsuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Tokuhiro Matsuno <tokuhirom@gmail.com>
---
+++ lib/Acme/CPANJ.pm
@@ -0,0 +1,39 @@
+package Acme::CPANJ;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::CPANJ - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::CPANJ;
+
+=head1 DESCRIPTION
+
+Acme::CPANJ is ...
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
